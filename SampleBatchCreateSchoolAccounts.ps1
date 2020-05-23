7cd3d4eab5cba315e974507d58c640bd06eb1c3d
taando-msft <42635690+taando-msft@users.noreply.github.com>
2020-05-23 23:58:43 +0900

    Add files via upload

---
+++ SampleBatchCreateSchoolAccounts.ps1
@@ -0,0 +1,7 @@
+﻿# accounts 一括指定してアカウント登録をするためのスクリプト
+.\CreateSchoolAccounts.ps1 0101_XXXX小学校_Student.csv
+.\CreateSchoolAccounts.ps1 0101_XXXX小学校_Teacher.csv
+.\CreateSchoolAccounts.ps1 0102_XXXX小学校_Student.csv
+.\CreateSchoolAccounts.ps1 0102_XXXX小学校_Teacher.csv
+.\CreateSchoolAccounts.ps1 0103_XXXX小学校_Student.csv
+.\CreateSchoolAccounts.ps1 0103_XXXX小学校_Teacher.csv
