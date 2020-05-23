0325757edc7d67f3e355e0567015002d40f1fcba
taando-msft <42635690+taando-msft@users.noreply.github.com>
2020-05-23 23:57:52 +0900

    Delete BatchCreateSchoolAccounts.ps1

--- BatchCreateSchoolAccounts.ps1
+++
@@ -1,7 +0,0 @@
-﻿# accounts 一括指定してアカウント登録をするためのスクリプト
-.\CreateSchoolAccounts.ps1 0101_XXXX小学校_Student.csv
-.\CreateSchoolAccounts.ps1 0101_XXXX小学校_Teacher.csv
-.\CreateSchoolAccounts.ps1 0102_XXXX小学校_Student.csv
-.\CreateSchoolAccounts.ps1 0102_XXXX小学校_Teacher.csv
-.\CreateSchoolAccounts.ps1 0103_XXXX小学校_Student.csv
-.\CreateSchoolAccounts.ps1 0103_XXXX小学校_Teacher.csv
